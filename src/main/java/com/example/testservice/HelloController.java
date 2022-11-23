7861fa4457aa666e9e052248bbd6f261929f38bd
Kseniia Bashkova <bashkkse@fel.cvut.cz>
2022-11-23 19:47:34 +0100

    init

---
+++ src/main/java/com/example/testservice/HelloController.java
@@ -0,0 +1,14 @@
+package com.example.testservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+	@GetMapping("/")
+	public String index() {
+		return "HIIIIIII from Spring Boot!";
+	}
+
+}
